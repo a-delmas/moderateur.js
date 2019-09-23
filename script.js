eebe532377c22ccbd77bcebbf64b0e4eab757e49
tomsirat <sirat.tom@outlook.com>
2019-09-23 14:29:14 +0200

    ok

--- script.js
+++ script.js
@@ -13,15 +13,27 @@ var textarea = document.getElementById("text");
 var result = document.getElementById("result");
 var btn = document.getElementsByClassName("btn")[0];
 var smiley = [":)", ":)", ":D"];
+i = 0;
 
 btn.addEventListener("click", function(){
     var text = textarea.value;
+    
+
+    text = text.replace(":)", "--")
+    
+    // if (document.getElementById(text) == smiley[i]) {
+    //     result.innerHTML == textarea.value.replace(smiley, "--");
+    // }
+    
+    
     result.innerHTML = text;
-    console.log(text);
+
 });
 
-textarea=textarea.replace(smiley,'--')
-console.log(textare);
+
+
+// textarea=textarea.replace(smiley,'--')
+// console.log(textare);
 
 
 //  for (i == smiley.length) {
