188b197b7e31b70943a624130e1101c87d89eb2c
a-delmas <aurore.delmas@hotmail.fr>
2019-09-23 11:57:15 +0200

    Modifs 1

---
+++ script.js
@@ -0,0 +1,11 @@
+var smiley = [":)", ":)", ":D"];
+var rest = str.replace("smiley", "--");
+var btn = document.getElementsByClassName("btn") [0];
+var result = document.getElementById("result");
+
+document.getElementById("text");
+div.innerHTML = "--";
+
+btn.addEventListener("click", function(){
+    result.value = "rest";
+});
