bf41b359f5190faabd236cc49659492aa4fcb288
a-delmas <aurore.delmas@hotmail.fr>
2019-09-23 13:58:13 +0200

    Modifs  2

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-var smiley = [":)", ":)", ":D"];
-var btn = document.getElementsByClassName("btn")[0];
-var result = document.getElementById("result");
+// var smiley = [":)", ":)", ":D"];
+// var btn = document.getElementsByClassName("btn")[0];
+// var result = document.getElementById("result");
+// var textarea = document.getElementById("text");
+// var i = 0;
+
+// btn.addEventListener("click", function() {
+//     getElementById("textarea").value;
+
+// });
+
 var textarea = document.getElementById("text");
-var i = 0;
+var result = document.getElementById("result");
+var btn = document.getElementsByClassName("btn")[0];
+var smiley = [":)", ":)", ":D"];
+
+btn.addEventListener("click", function(){
+    var text = textarea.value;
+    result.innerHTML = text;
+    console.log(text);
+});
+
+textarea=textarea.replace(smiley,'--')
+console.log(textare);
 
 
- for (i == smiley.length) {
-    if (textarea.value == smiley) {
-        console.log(smiley[i], "plop");
+//  for (i == smiley.length) {
+//     if (textarea.value == smiley) {
+//         console.log(smiley[i], "plop");
     
 
 
-}
-i++;
-}
+// }
+// i++;
+// }
 
 
 
